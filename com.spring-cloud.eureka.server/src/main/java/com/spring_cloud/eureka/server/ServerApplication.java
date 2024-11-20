ef2e69703593240cc10cf1119a4048623a128947
strongcookdas <goodjav141592@gmail.com>
2024-11-20 15:37:02 +0900

    chore : server properties 파일 설정 및 서버 등록

--- com.spring-cloud.eureka.server/src/main/java/com/spring_cloud/eureka/server/ServerApplication.java
+++ com.spring-cloud.eureka.server/src/main/java/com/spring_cloud/eureka/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.spring_cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // 유레카 서버로 동작
 public class ServerApplication {
 
 	public static void main(String[] args) {
